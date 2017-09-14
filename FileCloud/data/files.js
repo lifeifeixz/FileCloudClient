71a70a709a6b6def24ba812caeebaa54f2e75bdf
李飞飞 <lifeifeixz@sina.cn>
2017-09-14 17:49:02 +0800

    首次提交

---
+++ FileCloud/data/files.js
@@ -0,0 +1,72 @@
+var file = {
+	"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/",
+	"fileName": "Coupon",
+	"size": 0,
+	"sons": [{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/META-INF",
+			"fileName": "META-INF",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/FileExplorer",
+			"fileName": "FileExplorer",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/WEB-INF",
+			"fileName": "WEB-INF",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/fileExplorer2",
+			"fileName": "fileExplorer2",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/travel_event",
+			"fileName": "travel_event",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/H5model",
+			"fileName": "H5model",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/coupon",
+			"fileName": "coupon",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/base",
+			"fileName": "base",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 09:55:52"
+		},
+		{
+			"path": "/tomcat/apache-tomcat-8.0.46/webapps/Coupon/upload",
+			"fileName": "upload",
+			"size": 0,
+			"isDirectory": true,
+			"createDate": "2017-09-13 10:06:45"
+		}
+	],
+	"parentPath": "/tomcat/apache-tomcat-8.0.46/webapps",
+	"isDirectory": true,
+	"createDate": "2017-09-13 09:55:52"
+}
